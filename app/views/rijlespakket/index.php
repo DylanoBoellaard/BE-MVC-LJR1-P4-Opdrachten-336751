e51983d8c39fde4380421c47a014268fc4b90b7c
DylanoBoellaard <336751@student.mboutrecht.nl>
2023-05-25 14:25:49 +0200

    Created and finished rijlespakketen page

---
+++ app/views/rijlespakket/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../../public/css/instructeurGlobal.css">
+    <link rel="stylesheet" href="../../../public/css/instructeurHome.css">
+</head>
+<body>
+    <main>
+        <h3><?= $data['title'] ?></h3>
+
+        <table>
+            <thead>
+                <th>Naam</th>
+                <th>Mobiel</th>
+                <th>Pakketnaam</th>
+                <th>Aantal Lessen</th>
+                <th>Prijs</th>
+                <th>Startdatum</th>
+                <th>Datum rijbewijs behaald</th>
+            </thead>
+            <tbody>
+                <?= $data['tableRows']; ?>
+            </tbody>
+        </table>
+
+        <a href="<?= URLROOT ?>/home/index">Terug naar homepage</a>
+    </main>
+</body>
+</html>
